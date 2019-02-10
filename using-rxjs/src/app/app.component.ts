87a4f625a1f292586b1b2fe264fa4d316b7279a0
Irek Budzowski <irek.budzowski@gmail.com>
2019-02-10 17:23:58 +0100

    Add using-rxjs repo

--- using-rxjs/src/app/app.component.ts
+++ using-rxjs/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Posts</h1>
+    <ul>
+      <li *ngFor="let post of posts | async">
+        {{ post.id}} - {{ post.title }}
+      </li>
+    </ul>
+  `,
 })
-export class AppComponent {
-  title = 'using-rxjs';
+export class AppComponent implements OnInit{
+  posts: Observable<Post[]> ;
+
+  constructor(private  httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.posts = this.httpClient.get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+  }
+}
+
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
 }
